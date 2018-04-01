c566af3566e0968f1cb2b216b2ddc2614e1a4598
bigyankarki <b.gyankarki@gmail.com>
2018-03-31 20:31:33 -0400

    added events

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAANypBZAq9R0BACKhhlLphBgn8qTvcGAPOlErgqsgyTAmnZAQll2yZBiMGPRagA21Glet6WUgsZAjWSUwoSuDlMnQZAO3rW7d8Wwso2AaDj0bM4NKw4u6Mz4isCXPZANlJf5onZAJen90ZBPvEceovsAlSOnnUVv1AJgvW0I9zgaDAZDZD',
+    FB_PAGE_TOKEN: 'EAACIQZCZC9VPMBAJzIGqOuKv0jke4rjbz2ZCnmFiP34LAJn1nWfj0WRZAvUL00uvfLY017nZAzgZCMT7raXUZBdjseQklHdtvb3zOWwZBTPg9jgumh0tf1VsFkfyrutytACYsGVmWBfZAG6UHEsvAZBgDrcg91rnEvyKtZBRQnWAPeO6gZDZD',
     FB_VERIFY_TOKEN: 'this_is_a_test',
     API_AI_CLIENT_ACCESS_TOKEN: '00c8d5a8d6c041d5b527034847d3f64d',
-    FB_APP_SECRET: '7902c414e183c54786fbeb61523d1a5c',
-    // SERVER_URL: "https://cougarapp.herokuapp.com/",
-    SERVER_URL: "https://qkbdfhoazk.localtunnel.me/",
+    FB_APP_SECRET: 'f3de2add0c4cfaab92962e3b2f41c25e',
+    SERVER_URL: "https://cougarapp.herokuapp.com/"
 };
