d408b9ccee7ea7486636d8515ba43f6eb6b2de11
Aamosh <aamoshmacbook@Aamoshs-MacBook-Pro.local>
2018-06-26 13:27:47 -0400

    Hid the keys

--- config/config.js
+++ config/config.js
@@ -4,15 +4,14 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
     ADMIN_ID: '',
-    // SERVER_URL: "https://cougarapp.herokuapp.com/",
-    SERVER_URL: "https://lovely-firefox-80.localtunnel.me",
+    SERVER_URL: "",
 
 
     //  firbase config
-    API_KEY: "AIzaSyBkZx09MF5VY0pxn9KfZAgtTvH6wdNmiWk",
-    AUTH_DOMAIN: "cougarbot-site.firebaseapp.com",
-    DATABASE_URL: "https://cougarbot-site.firebaseio.com",
-    PROJECT_ID: "cougarbot-site",
-    STORAGE_BUCKET: "cougarbot-site.appspot.com",
-    MESSAGING_SENDER_ID: "1050730162532"
+    API_KEY: "",
+    AUTH_DOMAIN: "",
+    DATABASE_URL: "",
+    PROJECT_ID: "",
+    STORAGE_BUCKET: "",
+    MESSAGING_SENDER_ID: ""
 };
