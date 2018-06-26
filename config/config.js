a431dae72fc5a1128fde166aed34ee061b571e97
Aamosh <aamoshmacbook@Aamoshs-MacBook-Pro.local>
2018-06-26 13:23:35 -0400

    Hid the config keys

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAANypBZAq9R0BAFZARUoA5kiozMmIY08AU28ZBfo97nWmTlAb36ZAIxuTf9abWZBbADPayXa3YaeN3rkZCaiTkyEt2SA9yzF9udhUh2zenLZBVQmOPa6I7zsKb46v6DfZCP9yPKOPQCZC2xNxKwuSd88NOXbpsIAS4hKSNFZBXbq1J3QZDZD',
-    FB_VERIFY_TOKEN: 'this_is_a_test',
-    API_AI_CLIENT_ACCESS_TOKEN: '00c8d5a8d6c041d5b527034847d3f64d',
-    FB_APP_SECRET: '7902c414e183c54786fbeb61523d1a5c',
-    ADMIN_ID: '1905377426202174',
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_SECRET: '',
+    ADMIN_ID: '',
     // SERVER_URL: "https://cougarapp.herokuapp.com/",
     SERVER_URL: "https://lovely-firefox-80.localtunnel.me",
 
