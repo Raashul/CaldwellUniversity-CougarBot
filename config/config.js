5b4eb9998dd327a49107a4a040c2c8e09618577d
bigyankarki <b.gyankarki@gmail.com>
2018-07-06 22:56:03 -0400

    updated config

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     FB_APP_SECRET: '7902c414e183c54786fbeb61523d1a5c',
     ADMIN_ID: '1905377426202174',
     // SERVER_URL: "https://cougarapp.herokuapp.com/",
-    SERVER_URL: "https://clever-wombat-68.localtunnel.me",
+    SERVER_URL: "https://cougarbot-test.herokuapp.com/",
     APP_ACCESS_TOCKEN_COUGARBOT_TEST1 : '970474006443293|FIEF2eRZDR8Pm1bU0IIdg4SUlZA',
     APP_ACCESS_TOCKEN_COUGARBOT : '149825638978803|4jhm7d4cHorhm3ZqiIcvWLbofak',
 
